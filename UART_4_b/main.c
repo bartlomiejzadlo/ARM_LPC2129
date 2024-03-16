e8f9ec300e2e48b800bb7d2e27f497b90c32bd30
bartlomiejzadlo <162474274+bartlomiejzadlo@users.noreply.github.com>
2024-03-16 16:59:02 +0100

    Add files via upload

---
+++ UART_4_b/main.c
@@ -0,0 +1,62 @@
+#include "uart.h"
+#include "led.h"
+#include "servo.h"
+#include "keyboard.h"
+
+#define NULL 0
+
+int main (){
+
+	extern char cOdebranyZnak;
+	unsigned char ucPozycjaWskaznika;
+	
+	ServoInit(50);
+	UART_InitWithInt(9600);
+
+	LedInit();
+	
+
+	while(1){
+		
+				
+		switch(cOdebranyZnak){
+			
+			case '1':
+				ucPozycjaWskaznika++;
+				ServoGoTo(12*ucPozycjaWskaznika);
+				cOdebranyZnak=NULL;
+			break;
+			
+			case 'c':
+				ServoCallib();
+				ucPozycjaWskaznika = 0;
+				cOdebranyZnak=NULL;
+			break;
+			
+			default:{}
+			
+		}
+		
+		switch(eKeyboardRead()){
+			
+			case BUTTON_0:
+					ServoCallib();
+			break;
+			
+			case BUTTON_1:
+					ServoGoTo(12);
+			break;
+			
+			case BUTTON_2:
+					ServoGoTo(24);
+			break;
+			
+			case BUTTON_3:
+					ServoGoTo(36);
+			break;
+			
+			case RELASED:
+			break;
+		}
+	}
+}
